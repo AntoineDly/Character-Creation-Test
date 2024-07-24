9b59c7b564ac79d1b515cecb9b118d900a40cd9b
AntoineDly <antoine.delaunay333@gmail.com>
2024-07-23 19:09:46 -0700

    add users authentication

---
+++ app/Middleware/Cors.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+final class Cors
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  Closure(Request): (Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        return $next($request)
+            ->header('Access-Control-Allow-Origin', '*')
+            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+            ->header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, X-Token-Auth, Authorization');
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+use App\Character\Controllers\CreateCharacterController;
+use App\Character\Controllers\GetCharacterController;
+use App\Game\Controllers\CreateGameController;
+use App\Game\Controllers\GetGameController;
+use App\User\Controllers\AuthenticationController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:api');
+
+Route::post('/register', [AuthenticationController::class, 'register']);
+Route::post('/login', [AuthenticationController::class, 'login']);
+
+Route::middleware('auth:api')->group(function () {
+    Route::post('/logout', [AuthenticationController::class, 'logout']);
+
+    Route::get('/characters', [GetCharacterController::class, 'getCharacters']);
+    Route::get('/characters/with_game', [GetCharacterController::class, 'getCharactersWithGame']);
+    Route::get('/characters/{id}', [GetCharacterController::class, 'getCharacter']);
+    Route::get('/characters/{id}/with_game', [GetCharacterController::class, 'getCharacterWithGame']);
+    Route::post('/characters', [CreateCharacterController::class, 'createCharacter']);
+
+    Route::get('/games', [GetGameController::class, 'getGames']);
+    Route::get('/games/{id}', [GetGameController::class, 'getGame']);
+    Route::post('/games', [CreateGameController::class, 'createGame']);
+});

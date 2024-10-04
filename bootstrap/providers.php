8cb34f1f96572ba1529b7e3e85a86a58bba670a0
AntoineDly <antoine.delaunay333@gmail.com>
2024-10-03 20:23:10 -0700

    fix larastan factory

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,11 +3,9 @@
 declare(strict_types=1);
 
 use App\Providers\ControllersServiceProvider;
-use App\Providers\FactoriesServiceProvider;
 use App\Providers\RepositoriesServiceProvider;
 
 return [
     ControllersServiceProvider::class,
     RepositoriesServiceProvider::class,
-    FactoriesServiceProvider::class,
 ];
